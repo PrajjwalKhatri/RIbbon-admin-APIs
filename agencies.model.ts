861fe77b3ff03d3191c56cbbea1f854cfae4be1f
PrajjwalKhatri <82880367+PrajjwalKhatri@users.noreply.github.com>
2021-05-17 23:44:45 +0530

    Add files via upload

---
+++ agencies.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false, mongodb: {collection:"agencies" }}})
+export class Agencies extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  ID?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  agencyName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  website: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  agencyId: string;
+  
+  @property({
+    type: 'number',
+    required: true,
+  })
+  productCount: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Agencies>) {
+    super(data);
+  }
+}
+
+export interface AgenciesRelations {
+  // describe navigational properties here
+}
+
+export type AgenciesWithRelations = Agencies & AgenciesRelations;
